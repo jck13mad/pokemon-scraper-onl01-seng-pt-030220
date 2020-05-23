1f31e3de74c6228433795bad8e6ce1f8bd696bfe
Jack Madden <jck13mad@gmail.com>
2020-05-23 17:01:59 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -25,7 +25,10 @@ class Pokemon
     SQL
     
     pokemon = db.execute(sql, [id]).flatten
-    Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3])
+    Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3], db)
+  end
+  
+  
   
   
   
