56dceb21654445762ff25765e4ef3741dd5ce8e2
Jack Madden <jck13mad@gmail.com>
2020-05-23 16:57:34 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,14 @@ class Pokemon
   attr_accessor :name, :type, :id, :db, :hp 
   
   def initialize(id, name, type, hp, db)
-    
+    @id = id,
+    @name = name,
+    @type = type,
+    @hp = hp,
+    @db = db
+  end
+  
+  
   
   
   
